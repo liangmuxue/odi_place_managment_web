da7a52bab0d3e457df995b3084c8993095bed588
Eric-Lee-1 <249339031@qq.com>
2025-06-05 09:31:42 +0800

    爱立信修改ip

--- public/static/config.js
+++ public/static/config.js
@@ -2,6 +2,6 @@ let config = {
   // serverIP: "http://192.168.0.98:8101/hiCar"
   // serverIP: "http://59.110.222.2:8101/hiCar"
   // serverIP: "https://jizhi-pay.ss-cas.com:4433/hiCar"
-  // serverIP: "http://218.60.119.172:6379/hiCar"
-  serverIP: "http://218.61.32.171:6379/hiCar"
+  serverIP: "http://218.60.119.172:6379/hiCar"
+  // serverIP: "http://218.61.32.171:6379/hiCar"
 }
