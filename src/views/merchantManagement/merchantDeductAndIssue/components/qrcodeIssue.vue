d2f10f17d58fcc598a6776efa58d05a3288a95ac
Wang Xiaochong <softkingboy@vip.qq.com>
2025-12-10 09:34:38 +0800

    feat: implement merchant deduct and issue functionality with tabbed interface for manual and QR code issuance

---
+++ src/views/merchantManagement/merchantDeductAndIssue/components/qrcodeIssue.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="qrcodeIssue">
+    <!-- 二维码发放内容 -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "qrcodeIssue",
+  data() {
+    return {};
+  },
+  methods: {}
+};
+</script>
+
+<style lang="scss" scoped>
+.qrcodeIssue {
+  padding: 20px;
+}
+</style>
--- src/views/merchantManagement/merchantDeductAndIssue/index.vue
+++ src/views/merchantManagement/merchantDeductAndIssue/index.vue
@@ -1,109 +1,76 @@
 <template>
-  <div>
-    <div class="merchantDeductAndIssue_main ">
-      <el-form :model="newList" :rules="rules" ref="licensePlateForm">
-        <div class="base_dialog_main_content">
-          <div class="base_dialog_main_left" style="padding:100px">
-            <span class="base_dialog_condit">
-              <el-form-item label="输入方式">
-                <el-radio-group v-model="keyboardType">
-                  <el-radio label="1">键盘</el-radio>
-                  <el-radio label="2">软键盘</el-radio>
-                </el-radio-group>
-              </el-form-item>
-            </span>
-            <span class="base_dialog_condit">
-              <el-form-item label="车牌号" prop="licensePlate">
-                <el-input
-                  v-model="newList.licensePlate"
-                  placeholder="输入车牌号"
-                  style="width: 72%"
-                  class="filter-item"
-                  size="small"
-                />
-                <div
-                  class="inputBox"
-                  @click="clickInput"
-                  v-show="keyboardType == 2"
-                >
-                  <span v-if="newList.licensePlate">{{
-                    newList.licensePlate
-                  }}</span>
-                  <span v-else style="color:#ccc;font-size: 13px;"
-                    >输入车牌号</span
-                  >
-                </div>
-              </el-form-item>
-            </span>
-            、
-          </div>
-        </div>
-      </el-form>
+  <div class="merchantDeductAndIssue">
+    <div class="tab_top">
+      <div
+        :class="activeTab == 1 ? 'tab_top_btn' : 'tab_top_btn2'"
+        @click="changeTab(1)"
+      >
+        人工发放
+      </div>
+      <div
+        :class="activeTab == 2 ? 'tab_top_btn' : 'tab_top_btn2'"
+        @click="changeTab(2)"
+      >
+        二维码发放
+      </div>
     </div>
-    <Dialog ref="dialog" @setNo="setNo"></Dialog>
+    <div class="tab_content">
+      <ManualIssue v-if="activeTab == 1" />
+      <QrcodeIssue v-if="activeTab == 2" />
+    </div>
   </div>
 </template>
 
 <script>
-import Dialog from "./components/dialog";
+import ManualIssue from "./components/manualIssue";
+import QrcodeIssue from "./components/qrcodeIssue";
 
 export default {
   name: "merchantDeductAndIssue",
-  components: { Dialog },
+  components: { ManualIssue, QrcodeIssue },
 
   data() {
     return {
-      newList: {
-        licensePlate: ""
-      },
-      keyboardType: "1",
-      rules: {
-        licensePlate: [
-          { required: true, message: "请输入车牌号", trigger: "blur" }
-        ]
-      }
+      activeTab: 1
     };
   },
   methods: {
-    clickInput() {
-      let licensePlate = this.newList.licensePlate;
-      this.$refs.dialog.openDialog(licensePlate);
-    },
-    setNo(licensePlate) {
-      this.newList.licensePlate = licensePlate;
-      this.$refs["licensePlateForm"].validateField("licensePlate");
-    },
-    confirm() {
-      if (this.licensePlateUnit.length >= 7) {
-        console.log("车牌是：" + this.licensePlateUnit.join(""));
-        this.licensePlateDoor = false;
-      }
+    changeTab(tab) {
+      this.activeTab = tab;
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.merchantDeductAndIssue_main {
-  margin: 0 auto;
-  width: 800px;
-  .base_dialog_condit {
-    position: relative;
-    .inputBox {
-      position: absolute;
-      top: 4px;
-      left: 101px;
-      display: inline-block;
-      width: 71%;
-      height: 28px;
-      line-height: 30px;
-      padding: 0 15px;
-      font-size: 13px;
-      // border: 1px solid #dcdfe6;
-      color: #606266;
-      background: #fff;
-      border-radius: 5px;
-    }
+.merchantDeductAndIssue {
+  .tab_top {
+    height: 40px;
+    padding: 5px 10px;
+    display: flex;
+  }
+  .tab_top_btn {
+    height: 30px;
+    line-height: 30px;
+    width: 100px;
+    color: #037659;
+    text-align: center;
+    margin: 0 10px;
+    border-bottom: 2px solid #037659;
+    cursor: pointer;
+  }
+  .tab_top_btn2 {
+    height: 30px;
+    line-height: 30px;
+    width: 100px;
+    color: #303133;
+    text-align: center;
+    margin: 0 10px;
+    border-bottom: 2px solid transparent;
+    cursor: pointer;
+  }
+  .tab_content {
+    padding: 10px;
   }
 }
 </style>
