b67a26b7d37ad67285490076e29d603cc6fa1888
Eric-Lee-1 <249339031@qq.com>
2025-12-23 11:06:27 +0800

    商户合并

--- public/static/config.js
+++ public/static/config.js
@@ -5,10 +5,10 @@ let config = {
   // serverIP: "https://hicar.hichanceassets.cn:4321/hiCarWeb"
   // serverIP: "http://218.60.119.172:6379/hiCar"
   // serverIP: "http://218.61.32.171:8111/hiCar"
-  // serverIP: "http://218.61.32.171:8101/hiCar"
+  serverIP: "http://218.61.32.171:8101/hiCar"
   // serverIP: "http://218.60.95.37:8101/hiCar"
   // serverIP: "http://218.60.95.37:8111/hiCar"
   // serverIP: "http://218.60.117.168:8101/hiCar"
   // serverIP: "http://218.60.117.168:8111/hiCar"
-  serverIP: "http://218.61.32.173:8111/hiCar"
+  // serverIP: "http://218.61.32.173:8111/hiCar"
 }
